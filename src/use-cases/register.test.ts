e5d6c642ab47c1bc5fb9f5346bd987a0eaac4f49
Tamicktom <rickpokemon38@gmail.com>
2024-09-09 20:02:51 -0300

    add vitest

---
+++ src/use-cases/register.test.ts
@@ -0,0 +1,6 @@
+import { describe, expect, it, test } from "vitest";
+import { RegisterUseCase } from "./register";
+
+test("check if it works", async () => {
+  expect(true).toBe(true);
+});
---
+++ vite.config.mjs
@@ -0,0 +1,9 @@
+//* Libraries imports
+import { defineConfig } from "vitest/config";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [
+    tsConfigPaths(),
+  ]
+});
